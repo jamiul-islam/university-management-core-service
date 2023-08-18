cf38fd5083cd4ead8450b7080d9bcf92777dbc2d
jamiul-islam <jamiulislamjami@hotmail.com>
2023-08-18 16:45:56 +0600

    added global request handler

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,26 +1,19 @@
 import { AcademicSemester } from '@prisma/client';
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import { academicSemesterService } from './academicSemester.service';
 
-const insertIntoDB = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const result = await academicSemesterService.insertIntoDB(req.body);
-    sendResponse<AcademicSemester>(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Academic Semester created successfully',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await academicSemesterService.insertIntoDB(req.body);
+  sendResponse<AcademicSemester>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semester created successfully',
+    data: result,
+  });
+});
 
 export const academicSemesterController = {
   insertIntoDB,

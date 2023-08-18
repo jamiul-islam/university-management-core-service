9328e698c741fb8db988889790a96ac3e672dc64
jamiul-islam <jamiulislamjami@hotmail.com>
2023-08-18 16:14:28 +0600

    MVC structure added

---
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -0,0 +1,27 @@
+import { AcademicSemester } from '@prisma/client';
+import { NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+import sendResponse from '../../../shared/sendResponse';
+import { academicSemesterService } from './academicSemester.service';
+
+const insertIntoDB = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const result = await academicSemesterService.insertIntoDB(req.body);
+    sendResponse<AcademicSemester>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Academic Semester created successfully',
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const academicSemesterController = {
+  insertIntoDB,
+};
